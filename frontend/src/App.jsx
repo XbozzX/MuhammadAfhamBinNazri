fa226f536e91b05438b6ff746c4e1cd287d2606f
XbozzX <afham821@gmail.com>
2024-11-13 11:47:52 +0800

    fix the 404 page issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,5 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import HomePages from "./Pages/Home.jsx";
-import ProjectPages from "./Pages/Project.jsx";
-import ContactPages from "./Pages/ContactPages.jsx";
 
 function App() {
   return <div>Hello</div>;
