b804af784ad06e35c6af2e65cc4ff6c85fcb76ee
XbozzX <afham821@gmail.com>
2024-12-04 23:50:23 +0800

    update css for mobile

--- frontend/src/components/WorkingPlaceholder.jsx
+++ frontend/src/components/WorkingPlaceholder.jsx
@@ -1,4 +1,7 @@
 import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function WorkingPlaceholder() {
   return (
