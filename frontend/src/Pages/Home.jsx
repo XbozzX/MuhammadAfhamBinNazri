8c82a9989f2d967419c411829b97abf4afed1aa1
XbozzX <afham821@gmail.com>
2024-12-04 13:02:01 +0800

    update mobile css

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -8,13 +8,13 @@ import PersonalImgPlaceholder from "../components/PersonalImgPlaceholder.jsx";
 
 function Home() {
   return (
-    <div style={{ backgroundImage: `url(${DarkBG})`, backgroundSize: "cover" }}>
+    <div style={{ backgroundImage: `url(${DarkBG})`, backgroundSize: "cover", backgroundRepeat: "no-repeat", minHeight: "100vh" }} className=" flex flex-col items-center">
       <NavBarComp />
       <br />
       <br />
-      <div className=" max-w-fit ml-auto mr-auto">
+      <div className=" max-w-xl mx-auto text-center mt-6 px-4">
         <PersonalImgPlaceholder />
-        <p className=" text-2xl text-white p-3">
+        <p className=" text-lg md:text-2xl text-white mt-4">
           Hello, I am Muhammad Afham,
           <br />
           an aspiring software developer with a strong passion for technology
@@ -28,15 +28,15 @@ function Home() {
       </div>
       <br />
       <br />
-      <div>
+      <div className=" w-full px-4 mt-8">
         <Skill />
       </div>
       <br />
       <br />
-      <div>
+      <div className=" w-full px-4 mt-8">
         <WorkingExperience />
       </div>
-      <div>
+      <div className=" w-full px-4 mt-8 mb-8">
         <br />
         <br />
         <Project />
